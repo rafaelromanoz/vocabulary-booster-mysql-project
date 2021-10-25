5f48c06c168f1c2a029805f59c7db95ab9b63c61
Rafael Romano <rafa-kun@hotmail.com>
2021-10-25 17:35:39 -0300

    :white_check_mark: adicionando requisito 9 ao 11
    vqv

---
+++ desafio11.sql
@@ -0,0 +1,3 @@
+SELECT t1.ContactName AS `Nome`, t1.Country AS `País`, COUNT(*) -1 AS `Número de compatriotas` FROM w3schools.customers AS t1, w3schools.customers AS t2
+WHERE t1.Country = t2.Country 
+GROUP BY t1.CustomerID HAVING `Número de compatriotas` > 0 ORDER BY `Nome`;
