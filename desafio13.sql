6d89b0fa1c13edb278176fd153eb75313c7c9bd7
Rafael Romano <rafa-kun@hotmail.com>
2021-12-15 14:08:24 -0300

    corrigindo readme e outras coisas

--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,3 @@
-SELECT ProductName AS `Produto`, Price AS `Preço` FROM w3schools.products AS p INNER JOIN w3schools.order_details AS d ON p.ProductID = d.ProductID WHERE d.Quantity > 80 ORDER BY `Produto`;
+SELECT ProductName AS `Produto`, Price AS `Preço` 
+FROM w3schools.products AS p INNER JOIN w3schools.order_details AS d ON p.ProductID = 
+d.ProductID WHERE d.Quantity > 80 ORDER BY `Produto`;
